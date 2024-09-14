daebe3349aa0d00aaa8463b7a7eb19b0f78d3060
knowave <kwaktaemin98@gmail.com>
2024-09-14 15:25:26 +0900

    mysql config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PlanModule } from './plan/plan.module';
 import { CommunityModule } from './community/community.module';
+import { MysqlModule } from './database/mysql/mysql.module';
 
 @Module({
-  imports: [UserModule, PlanModule, CommunityModule],
+  imports: [UserModule, PlanModule, CommunityModule, MysqlModule],
   controllers: [AppController],
   providers: [AppService],
 })
