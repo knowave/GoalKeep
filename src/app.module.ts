451e059dd1dc37d8e9078a8f7c2087d7718a7335
knowave <kwaktaemin98@gmail.com>
2024-09-14 17:03:32 +0900

    s3 module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { PlanModule } from './plan/plan.module';
 import { CommunityModule } from './community/community.module';
 import { MysqlModule } from './database/mysql/mysql.module';
+import { S3Module } from './s3/s3.module';
 
 @Module({
-  imports: [UserModule, PlanModule, CommunityModule, MysqlModule],
+  imports: [UserModule, PlanModule, CommunityModule, MysqlModule, S3Module],
   controllers: [AppController],
   providers: [AppService],
 })
