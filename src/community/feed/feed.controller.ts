9339cce64b7b4f9ff6ddafc32394f6295c2eff3d
knowave <kwaktaemin98@gmail.com>
2024-09-18 19:04:46 +0900

    create feed for controller

--- src/community/feed/feed.controller.ts
+++ src/community/feed/feed.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { FeedService } from './feed.service';
+import { CreateFeedDto } from './dto/create-feed.dto';
+import { CurrentUser } from 'src/common/decorators/user.decorator';
+import { User } from 'src/user/entities/user.entity';
+import { Feed } from '../entities/feed.entity';
 
 @Controller('feeds')
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
+
+  @Post('')
+  async createFeed(
+    @Body() createFeedDto: CreateFeedDto,
+    @CurrentUser() user: User,
+  ): Promise<Feed> {
+    return await this.feedService.createFeed(createFeedDto, user);
+  }
 }
