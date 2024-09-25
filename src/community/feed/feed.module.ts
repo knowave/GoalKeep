a5f25c5c50242f0c73223d990eb190d1d246439e
knowave <kwaktaemin98@gmail.com>
2024-09-25 17:01:33 +0900

    redis config module import

--- src/community/feed/feed.module.ts
+++ src/community/feed/feed.module.ts
@@ -4,9 +4,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Feed } from '../entities/feed.entity';
 import { UserModule } from 'src/user/user.module';
 import { S3Module } from 'src/s3/s3.module';
+import { RedisConfigModule } from 'src/database/redis/redis-config.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Feed]), UserModule, S3Module],
+  imports: [
+    TypeOrmModule.forFeature([Feed]),
+    UserModule,
+    S3Module,
+    RedisConfigModule,
+  ],
   providers: [FeedService],
   exports: [FeedService],
 })
