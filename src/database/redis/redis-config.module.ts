6fb26b02716dfd201f0f5470fd4867bb8537d5d6
knowave <kwaktaemin98@gmail.com>
2024-09-25 16:58:08 +0900

    redis module

---
+++ src/database/redis/redis-config.module.ts
@@ -0,0 +1,31 @@
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { InjectRedis, RedisModule } from '@liaoliaots/nestjs-redis';
+import { REDIS_HOST, REDIS_PORT } from 'src/common/env';
+import { RedisService } from './redis.service';
+import { Redis } from 'ioredis';
+
+@Module({
+  imports: [
+    RedisModule.forRoot({
+      config: {
+        host: REDIS_HOST,
+        port: +REDIS_PORT,
+      },
+    }),
+  ],
+  providers: [RedisService],
+  exports: [RedisService],
+})
+export class RedisConfigModule implements OnModuleInit {
+  private readonly logger = new Logger(RedisConfigModule.name);
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
+  async onModuleInit() {
+    try {
+      await this.redis.ping();
+      this.logger.log('Redis module connected');
+    } catch (err) {
+      this.logger.error(`Failed to connect to Redis: ${err}`);
+    }
+  }
+}
