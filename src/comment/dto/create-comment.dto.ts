33b23ef4b6e3a67f3df1fa7f7310b72843f6618d
knowave <kwaktaemin98@gmail.com>
2024-09-26 16:22:24 +0900

    feedId값이 없을 경우 return 해주는 message 내용 수정

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -6,7 +6,7 @@ export class CreateCommentDto {
   @MaxLength(500, { message: '댓글 내용은 최대 500자까지 입력이 가능합니다.' })
   content: string;
 
-  @IsNotEmpty({ message: 'feed의 Id값을 넣어주세요.' })
+  @IsNotEmpty({ message: 'feed의 Id값은 필수값입니다..' })
   feedId: string;
 
   @IsNotEmpty({ message: 'user가 없습니다.' })
