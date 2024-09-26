f422621fcc346b9eea23d08b167d2ed6d087e012
knowave <kwaktaemin98@gmail.com>
2024-09-26 16:48:19 +0900

    feed module과 comment module import

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { LikeController } from './like.controller';
 import { LikeRepository } from './like.repository';
+import { FeedModule } from 'src/feed/feed.module';
+import { CommentModule } from 'src/comment/comment.module';
 
 @Module({
+  imports: [FeedModule, CommentModule],
   providers: [LikeService, LikeRepository],
   controllers: [LikeController],
 })
